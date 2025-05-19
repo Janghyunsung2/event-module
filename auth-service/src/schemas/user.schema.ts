afb44ad068617fdb3af9642186f4a7294eefa362
Janghyunsung2 <tlswlfk@gmail.com>
2025-05-19 18:29:01 +0900

    docker-compose.yml

--- auth-service/src/schemas/user.schema.ts
+++ auth-service/src/schemas/user.schema.ts
@@ -12,7 +12,7 @@ export class User extends Document {
     @Prop({ required: true })
     name: string;
 
-    @Prop({ required: true })
+    @Prop({ required: true , unique: true})
     nickname: string;
 
     @Prop({ required: true , default: 'USER' }) // 기본값을 'user'로 설정
